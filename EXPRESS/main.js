2861cabbe79f0fb1979f1e39151e675fc83ec692
Krish0-42 <spsaikrishnan@gmail.com>
2024-09-03 15:47:47 +0530

    d3

--- EXPRESS/main.js
+++ EXPRESS/main.js
@@ -1,19 +1,31 @@
 var express = require("express");
 var app = express();
-
 app.get("/myname",(req,res)=>{
-res.json({"msg":"yourname"});
+    res.json({"msg":"yourname"});
 });
 
 app.post("/myname",(req,res)=>{
-	res.json({"msg":"post name"});
-	});
-
-app.post("/login",(req,res)=>{
-	let email = req['query']['email']
-res.json({"msg":"email"});
+    res.json({"msg":"your post name"});
 });
 
+app.post("/login",(req,res)=>{
+
+    let email = req['query']['email']
+    let pwd = req['query']['password']
+	let uname = req['query']['name']
+	let adrs = req['query']['address']
+    
+	console.log(email, pwd)
+
+	if(email == 'admin@gmail.com' && pwd == 'admin' && uname == 'kadmin' && adrs == '12, b st, c nagar, d 600 001'){
+        res.json({"msg":"You are correct"})
+    }else{
+        res.json({"msg":"You are Incorrect"})
+    }
+});
+
+
+
 app.listen(8080,()=>{
-	console.log("server started")
-})
+    console.log("Server Started")
+});
