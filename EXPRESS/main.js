d7eed22e9ba14d6b0eb9861c0832eafe22fcf0a4
Krish0-42 <spsaikrishnan@gmail.com>
2024-09-03 14:30:51 +0530

    d2

--- EXPRESS/main.js
+++ EXPRESS/main.js
@@ -5,6 +5,15 @@ app.get("/myname",(req,res)=>{
 res.json({"msg":"yourname"});
 });
 
+app.post("/myname",(req,res)=>{
+	res.json({"msg":"post name"});
+	});
+
+app.post("/login",(req,res)=>{
+	let email = req['query']['email']
+res.json({"msg":"email"});
+});
+
 app.listen(8080,()=>{
 	console.log("server started")
 })
