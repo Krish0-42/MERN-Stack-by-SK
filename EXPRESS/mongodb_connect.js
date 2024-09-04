9bc8072275985a2bb5f0ce124ef12d19fed6a41b
Krish0-42 <spsaikrishnan@gmail.com>
2024-09-04 16:10:34 +0530

    MongoDB

---
+++ EXPRESS/mongodb_connect.js
@@ -0,0 +1,30 @@
+// create a sep dir(folder) for this 
+// run npm i mongodb or npm install mongodb
+
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb+srv://saikrishnan209:Sairam_22@cluster0.bvxx4.mongodb.net/';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'sample_mflix';
+
+async function main() {
+  // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db(dbName);
+    const data = await db.collection('movies').find({}).toArray();
+    console.log(data);
+
+  // the following code examples can be pasted here...
+    return 'done.';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
---
+++ EXPRESS/office.js
@@ -0,0 +1,35 @@
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+const url = 'mongodb+srv://saikrishnan209:Sairam_22@cluster0.bvxx4.mongodb.net/';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'office';
+
+async function insertData() {
+
+    let empData = {
+        "name": "Krishna",
+        "mobile" : "9150867165",
+        "address" : "12, ABC St, DEF nagar, ghi 100 0001"
+    }
+
+  // Use connect method to connect to the server
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = await db.collection('employee');
+        await collection.insertOne(empData);
+        console.log("Inserted");
+
+  // the following code examples can be pasted here...
+    return 'done.';
+}
+
+insertData()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
+    
